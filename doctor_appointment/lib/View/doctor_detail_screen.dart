92d471b230d710865deb0d1695a4b151f5e9f5ef
=BaoLamIT1 <baolam2k3.contact@gmail.com>
2025-01-13 23:28:19 +0700

    Update doctor_detail_screen.dart

---
+++ doctor_appointment/lib/View/doctor_detail_screen.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class DoctorDetailScreen extends StatelessWidget {
+  const DoctorDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
